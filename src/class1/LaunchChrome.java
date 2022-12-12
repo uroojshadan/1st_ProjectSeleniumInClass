18e2b5e3442a3ca0d6ef969da5ce010cbbe22926
Urooj Shadan <uroojshadan.me@gmail.com>
2022-12-11 20:08:04 -0600

    commit

---
+++ src/class1/LaunchChrome.java
@@ -0,0 +1,33 @@
+package class1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		 System.setProperty("webdriver.gecko.driver", "driver/geckodriver");
+		 WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.chrome.driver", "/Users/comet/SeleniumWorkspace/driver/chromedriver");
+		//WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		Thread.sleep(2000);
+		// Thread.sleep(2000);//-->uncomment if using firefox
+		driver.findElement(By.xpath("//input[@name='btnK']")).click();
+		// Thread.sleep(2000);
+		//driver.close();// comment this line if using firefox
+		driver.quit();
+
+	}
+
+}
